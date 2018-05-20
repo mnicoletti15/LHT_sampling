44bea7f7f0509e90f3db5410d5a078bb90e8fe24
Matthew Nicoletti <mnicoletti@berkeley.edu>
2018-05-20 15:24:32 -0700

    debugging

--- main.cpp
+++ main.cpp
@@ -3,17 +3,20 @@
 #include <cmath>
 #include <random>
 #include "Tableaux.h"
+#include <ctime>       /* time */
 
 using namespace std;
 
-vector<int> step(long seed, vector<int>& lambda, vector<int>& mu, int n, vector <int> T, int length);
+void step(unsigned int seed, Tableaux& T);
 void transform(double r2, Tableaux& T, int i, int j);
+void print(Tableaux& T);
 
 
 int main() {
+    unsigned int seed = 1;
     int n = 5;
-    int height = 4;
-    int N = 6;
+    double u = .8;
+    double v = .25;
     double q = .5;
 
     vector<int> lambda ({6, 6, 4, 3});
@@ -26,16 +29,10 @@ int main() {
                    });
     vector<double> x;
 
-    Tableaux T(lambda, mu, t, n, .5, .2, .5);
-
-
-    for (unsigned i=0; i < height + 2; ++i) {
-        for (unsigned j = 0; j < N + 2; ++j) {
-            cout << T.get(i, j) << ' ';
-        }
-        cout << endl;
-    }
-
+    Tableaux T(lambda, mu, t, n, u, v, q);
+    print(T);
+    step(seed, T);
+    print(T);
     return 0;
 }
 
@@ -53,6 +50,7 @@ void step(unsigned int seed, Tableaux& T) {
                 if (r <= (double) k / T.length) {
                     double r2 = ((double) rand() / (RAND_MAX));
                     transform(r2, T, i, j);
+                    return;
                 }
             }
         }
@@ -63,6 +61,8 @@ void step(unsigned int seed, Tableaux& T) {
 void transform(double r2, Tableaux& T, int i, int j) {
     int k = T.min_val(i, j);
     double Z = T.partition_fn(i, j);
+    cout << T.get(0, 0) << endl;
+    cout << Z << endl;
     double c = T.face_weight(k, i, j)/Z;
     while (c < r2) {
         c += T.face_weight(++k, i, j)/Z;
@@ -70,3 +70,11 @@ void transform(double r2, Tableaux& T, int i, int j) {
     T.set(i, j, k);
 }
 
+void print(Tableaux& T) {
+    for (unsigned i=0; i < T.height + 2; ++i) {
+        for (unsigned j = 0; j < T.N + 2; ++j) {
+            cout << T.get(i, j) << ' ';
+        }
+        cout << endl;
+    }
+}
